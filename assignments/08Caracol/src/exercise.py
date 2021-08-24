8704f9479e898c6f581d0781e279d8bcb90e9f82
PedroCA01656607 <88863098+PedroCA01656607@users.noreply.github.com>
2021-08-23 20:50:41 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,8 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
+    minu=float(input('Dame los minutos: '))
 
-
-    print("Centímentros recorridos:",cm)
+    v=5.7*60*0.1
+    resultado=v*minu
+    cm=round(resultado,5)
+    print('Centímetros recorridos:',cm)
 
 
 if __name__ == '__main__':
